2ed1d17d696e614214a80699a30c7a5a4a75c85f
Christin-Ts8a <Christin.ts8a@gmail.com>
2023-11-15 00:23:03 -0800

    增加生成份额功能，修改构造函数

--- include/io/fluid_rss_client.h
+++ include/io/fluid_rss_client.h
@@ -13,11 +13,16 @@ class FluidRSSClient {
     FluidRSSClient();
     FluidRSSClient(int client_size, int client_id, int server_size)
         : client_size(client_size), client_id(client_id), server_size(server_size) {
+        // 计算默认阈值，保证诚实大多数
         this->threshold = server_size % 2 == 0 ? (server_size / 2 - 1) : (server_size / 2);
-        get_random_seeds(this->keys);
+        this->key_size = C(this->threshold, server_size);
+        // 给随机数种子申请对应空间
+        this->keys = new block[this->key_size];
+        // 生成随机数种子，数量与 server_size 对应
+        get_random_seeds(this->keys, this->key_size);
     }
     ~FluidRSSClient() {
-        free(this->keys);
+        delete this->keys;
     }
 
     // 获取随机数作为为随机数生成器的种子
@@ -29,7 +34,7 @@ class FluidRSSClient {
     }
 
     // 获取多个随机数作为为随机数生成器的种子
-    void get_random_seeds(block* seeds){
+    void get_random_seeds(block* seeds, int num = 1){
         int seed_num = C(this->threshold, this->server_size);
         uint32_t *seed_ptr = (uint32_t *)&seeds;
         random_device rand_div("/dev/urandom");
@@ -48,7 +53,7 @@ class FluidRSSClient {
     }
 
     // 获取int型数据
-    void get_dataset(string filepath, int* data, int num){
+    void get_dataset(string filepath, int* data, int data_num){
         ifstream file(filepath);
         if(!file.is_open()) {
             cout << "File can not be opend" << endl;
@@ -61,8 +66,8 @@ class FluidRSSClient {
             cout << "There is not enough data" << endl;
         }
 
-        data = (int*)malloc(sizeof(int) * num);
-        for(int i = 0; i < num; i++) {
+        data = new int[data_num];
+        for(int i = 0; i < data_num; i++) {
             *(data+i) = dataset[i].asInt();
         }
     }
@@ -75,8 +80,16 @@ class FluidRSSClient {
 
     // 生成数据对应的份额
     void get_shares_from_dataset(block* shares, int* data, int data_num){
+        shares = new block[data_num];
         for(int i = 0; i < data_num; i++) {
-            int  temp = *(data+i);
+            block temp_sum;
+            for(int j = 0; j < this->key_size; j++) {
+                block temp;
+                PRG prg(this->keys[j]);
+                prg.random_block(temp);
+                temp_sum -= temp;
+            }
+            shares[i] = data[i] - temp_sum;
         }
     }
 
@@ -94,5 +107,6 @@ class FluidRSSClient {
     int client_id;
     int server_size;
     int threshold;
+    int key_size;
     block* keys;
 };
